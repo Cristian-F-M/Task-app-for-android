3870bddc8c3356bdbaa80a9897597a5e37e42774
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-10-22 17:16:16 -0500

    Add App.js

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
+import { useEffect } from 'react'
 import { Main } from './Main'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { SQLiteProvider, useSQLiteContext } from 'expo-sqlite'
+import { useTasks } from './utils/task'
+import { TasksProvider } from './context/Task'
 
 export default function App() {
+  const { createTable, restarTable } = useTasks()
+
+  useEffect(() => {
+    createTable()
+  }, [])
+
   return (
     <SafeAreaProvider>
-      <Main />
+      <TasksProvider>
+        <SQLiteProvider
+          databaseName="TODO.db"
+          assetSource={{ assetId: require('./db/TODO.db') }}
+        >
+          <Main />
+        </SQLiteProvider>
+      </TasksProvider>
     </SafeAreaProvider>
   )
 }
