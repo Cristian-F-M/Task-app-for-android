bb24bac1488b85a0c637c9fc0bf5b6c4df5462ba
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-10-22 17:13:12 -0500

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
     './App.{js,jsx,ts,tsx}',
     './Main.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
-    './app/**/*.{js,jsx,ts,tsx}'
+    './app/**/*.{js,jsx,ts,tsx}',
+    './icons/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {}
